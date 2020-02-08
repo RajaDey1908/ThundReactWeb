5d8f146f3fe26190f8fa05e724ec5f0a61677263
RajaDey1908 <raja.dey1908@gmail.com>
2020-02-08 20:33:47 +0530

    auth function setup done

--- src/store/actions/actionsFirst.js
+++ src/store/actions/actionsFirst.js
@@ -1,7 +1,4 @@
 import * as actionTypes from '../types/types'
-import axios from 'axios';
-const API_URL = 'https://reqres.in/';
-
 
 export const title = ()=>{
     return{
--- src/store/types/types.js
+++ src/store/types/types.js
@@ -2,3 +2,7 @@ export const TITLE ="TITLE";
 export const SUBJECT ="SUBJECT";
 export const DETAILS ="DETAILS";
 export const DATE ="DATE";
+
+export const AUTH_START="AUTH_START"
+export const AUTH_SUCCESS="AUTH_SUCCESS"
+export const AUTH_FAIL="AUTH_FAIL"
